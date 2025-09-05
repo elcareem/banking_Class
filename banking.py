6d75ede2d7141a9098973f08161f97791c6e07f6
your_name <andrewmiracle100@gmail.com>
2025-09-05 17:34:25 +0100

    add deposit, withdraw & transfer functionality

--- banking.py
+++ banking.py
@@ -13,11 +13,38 @@ class BankAccount:
         if self.promo:
             self.balance = balance + promo_price 
         else:
-
+            self.balance = balance
         self.name = name
         self.acct_number = acct_number
-        self.balance = balance
 
-joy = BankAccount('Joy', 1000)
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            return "Success"
+        else:
+            return "Insufficient Funds"
+
+    def transfer(self, amount, recipient):
+        tx = self.withdraw(amount)
+        if tx == "Success":
+            recipient.deposit(amount)
+        else:
+            return tx
+
+
+joy = BankAccount('Joy', 5000)
+tom = BankAccount('Tom', 0)
 
 print(joy.balance, joy.promo)
+joy.deposit(1000)
+print(joy.balance)
+joy.withdraw(200)
+print(joy.balance)
+
+joy.transfer(1000, tom)
+
+print(tom.balance, tom.promo)
+
