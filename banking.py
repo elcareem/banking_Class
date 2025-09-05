7a89e56263f7164788d22263a003123953bd6375
your_name <andrewmiracle100@gmail.com>
2025-09-05 20:52:08 +0100

    add freeze feature

--- banking.py
+++ banking.py
@@ -1,50 +1,69 @@
 import random
 
 class BankAccount:
-    def __init__(self, name, balance):
+    def __init__(self, name, balance, isAdmin = False):
         promo_price = 2000
         acct_number = int(random.random() * 10000000000)
           
         if random.random() < 0.5:
-            self.promo = True
+            self.hasPromo = True
         else:
-            self.promo = False
+            self.hasPromo = False
 
-        if self.promo:
+        if self.hasPromo:
             self.balance = balance + promo_price 
         else:
             self.balance = balance
         self.name = name
         self.acct_number = acct_number
+        self.isFrozen = False
+        self.isAdmin = isAdmin
 
     def deposit(self, amount):
-        self.balance += amount
+        if not self.isFrozen:
+            self.balance += amount
+        else:
+            return "Can't Deposit, Account is frozen"
 
     def withdraw(self, amount):
-        if amount <= self.balance:
-            self.balance -= amount
-            return "Success"
+        if not self.isFrozen:
+            if amount <= self.balance:
+                self.balance -= amount
+                return "Success"
+            else:
+                return "Insufficient Funds"
         else:
-            return "Insufficient Funds"
+            return "Can't Withdraw, Account is frozen"
 
     def transfer(self, amount, recipient):
-        tx = self.withdraw(amount)
-        if tx == "Success":
-            recipient.deposit(amount)
+        if not self.isFrozen:
+            tx = self.withdraw(amount)
+            if tx == "Success":
+                recipient.deposit(amount)
+            else:
+                return tx
         else:
-            return tx
+            return "Can't Transfer, Account is frozen"
 
+    def freeze(self, account):
+        if self.isAdmin:
+            if not self.isFrozen:
+                self.isFrozen = True
+                return f"{account.name} is frozen"
+            else:
+                return "Account is already frozen"
+        else:
+            return "Cannot perform this action, require admin privilege"
 
-joy = BankAccount('Joy', 5000)
+# freeze(account)
+#unfree (account)
+#Admin True
+#isAdmin = False
+#isFreezed = False
+joy = BankAccount('Joy', 5000, True)
 tom = BankAccount('Tom', 0)
 
-print(joy.balance, joy.promo)
-joy.deposit(1000)
-print(joy.balance)
-joy.withdraw(200)
-print(joy.balance)
+print(joy.freeze(tom))
+print(tom.freeze(joy))
 
-joy.transfer(1000, tom)
-
-print(tom.balance, tom.promo)
-
+print(joy.transfer(1000, tom))
